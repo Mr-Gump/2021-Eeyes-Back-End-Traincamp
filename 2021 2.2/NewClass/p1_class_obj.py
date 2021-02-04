78c9a7201e4a5d2ccaa235e58c192c74c65c5ba7
Mr-Gump <54069747+Mr-Gump@users.noreply.github.com>
2021-02-04 20:53:26 +0800

    modify

---
+++ 2021 2.2/NewClass/p1_class_obj.py
@@ -0,0 +1,21 @@
+class Human(name,age):
+    
+
+
+# I have a dream
+class MyFirstClass:
+    pass
+
+a = MyFirstClass()
+b = MyFirstClass()
+
+# 不同内存地址，两个不同对象
+type(a)
+id(a)
+a.__class__()
+b.__class__()
+
+# 类也是对象
+c = MyFirstClass
+d = c()
+d.__class__()
---
+++ 2021 2.2/NewClass/test2.py
@@ -0,0 +1,2 @@
+import test
+print(test.__name__)
